6c60b8533844d25ecfa88662c6b9b93085b1c2e1
newage <vadim.leontiev@gmail.com>
2017-06-29 09:26:20 +0300

    Added modules to project

---
+++ angular-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+      new Recipe('A Test Recipe', 'This is a test recipe', 'https://vegaprocity.com/wp-content/uploads/2014/10/veganpizza3-750x420.jpg'),
+      new Recipe('A Test Recipe', 'This is a test recipe', 'https://vegaprocity.com/wp-content/uploads/2014/10/veganpizza3-750x420.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
